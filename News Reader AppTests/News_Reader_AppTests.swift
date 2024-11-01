78f15c5c7a809cab9374ce2b4f75ee61b23a735f
Advait Singh <Advaitsingh05@gmail.com>
2024-11-01 14:32:14 +0530

    Initial Commit

---
+++ News Reader AppTests/News_Reader_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  News_Reader_AppTests.swift
+//  News Reader AppTests
+//
+//  Created by Advait Singh on 01/11/24.
+//
+
+import Testing
+@testable import News_Reader_App
+
+struct News_Reader_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
